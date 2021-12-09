f563f31775c7bad409231c07887cded5ece2676e
lewlew08 <95668008+lewlew08@users.noreply.github.com>
2021-12-09 14:08:50 +0800

    Add notify expert blade

---
+++ resources/views/admin/notify_mail_to_expert.blade.php
@@ -0,0 +1,14 @@
+
+    <div class="container text-center m-1">
+        <h1 class="lead mt-3">Send mail using attachment</h1>
+        <p class="lead">   {{ $email_body }}</p>
+        <p class="lead">
+       
+  
+  
+  Please click  <a class="btn btn-success" href="{{ $response_url }}" >here</a> for your response.
+  
+ 
+        </p>
+  
+    </div>
